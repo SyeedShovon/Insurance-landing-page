677512078e251d9d2890d13538d1458f8fe0a7ce
SyeedShovon <shovoncsepstu@gmail.com>
2024-12-21 00:53:34 +0600

    Advertising landing page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      'header-blue': '#011733',
+        },
+  },
+  plugins: [],
+}
+
